20d30917fb24c23fb749b9168949cb982f1e531c
Robert Meredith <64670585+robertmeredith@users.noreply.github.com>
2023-09-12 15:22:15 +1000

    edited newRequest util

--- client/src/utils/newRequest.js
+++ client/src/utils/newRequest.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const newRequest = axios.create({
-  baseURL: 'http://localhost:5001/api/v1',
+  baseURL: '/api/v1',
+  // baseURL: 'http://localhost:5001/api/v1',
   withCredentials: true,
 })
 
